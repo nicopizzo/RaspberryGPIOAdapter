da298d73ffaf43c1790196e4f7ed3ff2b8de0455
Nico Pizzo <funkiernails@yahoo.com>
2018-09-25 23:19:03 -0400

    Added setup file for iot

---
+++ Scripts/setup_iot.ps1
@@ -0,0 +1,16 @@
+﻿Param(
+    [string]$server,
+    [string]$username,
+    [string]$password,
+    [string]$sharedFolder
+)
+
+$securePw = ConvertTo-SecureString $password -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($username, $securePw)
+
+$remoteCommand = {
+    param($sFolder)
+    FolderPermissions $sFolder -e
+}
+
+Invoke-Command -ComputerName $server -ScriptBlock $remoteCommand -ArgumentList $sharedFolder -Credential $cred
